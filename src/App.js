9dbbb882c8dccbb24d9f805ad37900e166f056d4
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2018-02-28 17:09:51 +0800

    晚上在看一下 在react中使用js来调用动画

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import test from './test.svg';
 import './App.css';
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -14,7 +15,13 @@ class App extends Component {
         <p>
           开始学习svg来构图并创建动画
         </p>
-        <img src={test} alt="test" />        
+        <img src={test} alt="test" />
+        <svg width="200" height="200" viewBox="0 0 200 200">
+          <line x1="10" y1="10" x2="110" y2="10" fill="none" strokeWidth="12" stroke="#666" strokeLinecap="round" />
+          <line x1="10" y1="10" x2="110" y2="10" fill="none" strokeWidth="12" stroke="#FC4D04" strokeDasharray="100" strokeDashoffset="20" strokeLinecap="round" />
+          <circle cx="60" cy="80" r="54" fill="none" stroke="#666" strokeWidth="12" strokeLinecap="round" />
+          <circle cx="60" cy="80" r="54" fill="none" stroke="#FC4D04" strokeWidth="12" strokeDasharray="339" strokeLinecap="round" />
+        </svg>
       </div>
     );
   }
