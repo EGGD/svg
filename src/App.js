3bfa5669f9eb5c8a9efda9b6f27897c0eef7bb8c
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2018-03-02 10:34:00 +0800

    右侧单个圆的组件封装完毕

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactSVG from 'react-svg';
+import Circle from './svg/Circle.js';
 import logo from './logo.svg';
 import test from './test.svg';
 import './App.css';
@@ -11,34 +11,34 @@ class App extends Component {
       svglineOneUnFillValue: parseInt(Math.random() * 255),
     }
   }
-  componentDidMount() {
-  //  let initSvgNumberSetTime= setTimeout(() => {
-      this.setSvgNumber();
-    // }, 1000);
-  }
-  setSvgNumber() {
-    let fillValue = this.state.svglineOneFillValue;
-    let unFillValue = this.state.svglineOneUnFillValue;
-    let setFillValue;
-    if (fillValue == unFillValue) {
-      this.setState({
-        svglineOneUnFillValue: parseInt(Math.random() * 100)
-      });
-    };
-    if (fillValue > unFillValue) {
-      setFillValue = Math.max(unFillValue, fillValue - 1);
-    } else {
-      setFillValue = Math.min(unFillValue, fillValue + 1);
-    };
-    this.setState({ svglineOneFillValue: setFillValue });
-    clearTimeout(timeout);        
-    let timeout = setTimeout(() => {
-      this.setSvgNumber();          
-    }, 20);
-  }
-  componentWillUnmount(){
-    clearTimeout(initSvgNumberSetTime);
-  }
+  // componentDidMount() {
+  // //  let initSvgNumberSetTime= setTimeout(() => {
+  //     //this.setSvgNumber();
+  //   // }, 1000);
+  // }
+  // setSvgNumber() {
+  //   let fillValue = this.state.svglineOneFillValue;
+  //   let unFillValue = this.state.svglineOneUnFillValue;
+  //   let setFillValue;
+  //   if (fillValue == unFillValue) {
+  //     this.setState({
+  //       svglineOneUnFillValue: parseInt(Math.random() * 100)
+  //     });
+  //   };
+  //   if (fillValue > unFillValue) {
+  //     setFillValue = Math.max(unFillValue, fillValue - 1);
+  //   } else {
+  //     setFillValue = Math.min(unFillValue, fillValue + 1);
+  //   };
+  //   this.setState({ svglineOneFillValue: setFillValue });
+  //   clearTimeout(timeout);        
+  //   let timeout = setTimeout(() => {
+  //     this.setSvgNumber();          
+  //   }, 20);
+  // }
+  // componentWillUnmount(){
+  //   clearTimeout(initSvgNumberSetTime);
+  // }
   render() {
     return (
       <div className="App">
@@ -50,17 +50,17 @@ class App extends Component {
           开始学习svg来构图并创建动画
         </p>
         <img src={test} alt="test" />
-        <svg width="100" height="100" viewBox="0 0 100 100">
-          {/* <line x1="10" y1="10" x2="90" y2="10" fill="none" strokeWidth="5" stroke="#666" strokeLinecap="round" />
-          <line x1="10" y1="10" x2="90" y2="10" fill="none" strokeWidth="5" stroke="#FC4D04" strokeDasharray="100" strokeDashoffset={this.state.svglineOneFillValue} strokeLinecap="round" /> */}
+        {/* <svg width="100" height="100" viewBox="0 0 100 100">
+          <line x1="10" y1="10" x2="90" y2="10" fill="none" strokeWidth="5" stroke="#666" strokeLinecap="round" />
+          <line x1="10" y1="10" x2="90" y2="10" fill="none" strokeWidth="5" stroke="#FC4D04" strokeDasharray="100" strokeDashoffset={this.state.svglineOneFillValue} strokeLinecap="round" />
           <text x="50%" y="50%" style={{textAnchor: "middle", dominantBaseline: "middle"}} >{this.state.svglineOneFillValue}</text>
           <circle cx="50%" cy="50%" r="40.65" fill="none" stroke="#666" strokeWidth="10" opacity="0.3" strokeLinecap="round" />
           <circle cx="50%" cy="50%" r="40.65" fill="none" stroke="#FC4D04" strokeWidth="10"  strokeDasharray="255"  strokeDashoffset={this.state.svglineOneFillValue} strokeLinecap="round" />
         </svg>
         <svg width="200" height="200" viewBox="0 0 200 200"  >
           <polygon  strokeDasharray="339" fill="none" stroke="#666" strokeDashoffset={this.state.svglineOneFillValue} points="66.2,41.2 38.8,91 64.2,153.8 154,155.5 175,90.8 135.5,38.8 "/>
-        </svg>
-
+        </svg> */}
+        <Circle svgCircleHeight="40" svgCircleWidth="40"  svgCircleColor="#FC4D04" svgCircleOneUnFillValue="200" svgCircleLineWidth="20" />
       </div>
     );
   }
---
+++ src/svg/Circle.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+class Circle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      circleInit: {
+        svgTextColor: '#000',        
+        svgCircleHeight:"100",
+        svgCircleWidth:"100",
+        svgCircleOneFillValue: 255,
+        svgCircleOneUnFillValue: parseInt(Math.random() * 255),
+        svgCircleColor: '#000',
+        svgCircleLineWidth: '10',
+        svgCircleOpacity: '0.3'
+      }
+    }
+  }
+  componentDidMount() {
+    this.svgInit()
+    console.log(this.state.circleInit);
+
+    this.setSvgNumber();
+  }
+  svgInit(){
+    let oldCircleInit = this.state.circleInit;
+    let newCircleInit = this.props;
+    for (const key in newCircleInit) {
+      if (oldCircleInit.hasOwnProperty(key)) {
+        oldCircleInit[key] = newCircleInit[key]
+      }
+    }
+  }
+  setSvgNumber() {
+    let circleInit = this.state.circleInit;
+    let fillValue = circleInit.svgCircleOneFillValue;
+    let unFillValue = circleInit.svgCircleOneUnFillValue;
+    let setFillValue;
+    if (fillValue == unFillValue) {
+      // this.setState({
+      //   svgCircleOneUnFillValue: parseInt(Math.random() * 100)
+      // });
+      return;
+    };
+    if (fillValue > unFillValue) {
+      setFillValue = Math.max(unFillValue, fillValue - 1);
+    } else {
+      setFillValue = Math.min(unFillValue, fillValue + 1);
+    };
+    circleInit.svgCircleOneFillValue = setFillValue;
+    this.setState({ circleInit: circleInit });
+    clearTimeout(timeout);
+    let timeout = setTimeout(() => {
+      this.setSvgNumber();
+    }, 20);
+  }
+  componentWillUnmount() {
+    // clearTimeout(initSvgNumberSetTime);
+  }
+  render() {
+    let circleInit = this.state.circleInit,
+        svgCircleWidth=circleInit.svgCircleWidth,
+        svgCircleHeight=circleInit.svgCircleHeight,
+        svgTextColor = circleInit.svgTextColor,
+        svgCircleColor = circleInit.svgCircleColor,
+        svgCircleOneFillValue = circleInit.svgCircleOneFillValue,
+        svgCircleLineWidth = circleInit.svgCircleLineWidth,
+        svgCircleOpacity = circleInit.svgCircleOpacity;
+    return (
+      <div>
+        <svg width={svgCircleWidth} height={svgCircleHeight} viewBox="0 0 100 100">
+          <text x="50%" y="50%" style={{ textAnchor: "middle", dominantBaseline: "middle", fill: svgTextColor }} >{255 - svgCircleOneFillValue}</text>
+          <circle cx="50%" cy="50%" r="40.65" fill="none" stroke="#666" strokeWidth={svgCircleLineWidth} opacity={svgCircleOpacity} strokeLinecap="round" />
+          <circle cx="50%" cy="50%" r="40.65" fill="none" stroke={svgCircleColor} strokeWidth={svgCircleLineWidth} strokeDasharray="255" strokeDashoffset={svgCircleOneFillValue} strokeLinecap="round" />
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default Circle;
